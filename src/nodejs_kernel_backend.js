c93d511eea2f215e46ff3a25371b8fa86619431a
Nick Kreeger <nick.kreeger@gmail.com>
2018-02-26 16:22:37 -0800

    Move over the new backend.

---
+++ src/nodejs_kernel_backend.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NodeJSKernelBackend = /** @class */ (function () {
+    function NodeJSKernelBackend() {
+    }
+    return NodeJSKernelBackend;
+}());
+exports.NodeJSKernelBackend = NodeJSKernelBackend;
