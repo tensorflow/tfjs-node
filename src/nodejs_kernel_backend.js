fe03075dd8434acd5b8c66a99941ac2cfd9ad19b
Nick Kreeger <nick.kreeger@gmail.com>
2018-02-26 16:27:35 -0800

    Binding test file WIP.

--- src/nodejs_kernel_backend.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var NodeJSKernelBackend = /** @class */ (function () {
-    function NodeJSKernelBackend() {
-    }
-    return NodeJSKernelBackend;
-}());
-exports.NodeJSKernelBackend = NodeJSKernelBackend;
