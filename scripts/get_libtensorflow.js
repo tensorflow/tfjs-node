fe0296f4b2a918e2996716a629d84ebd647aea4c
Nick Kreeger <nick.kreeger@gmail.com>
2018-08-02 09:54:27 -0700

    save

--- scripts/get_libtensorflow.js
+++ scripts/get_libtensorflow.js
@@ -1,3 +1,8 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const tar = ('tar');
+
 console.log('argv: ', process.argv);
 
 const BASE_URI = 'https://storage.googleapis.com/tf-builds/';
@@ -9,6 +14,7 @@ const targetDir = process.argv[2];
 const platform = process.argv[3];
 
 let targetUri = BASE_URI;
+let targetPath;
 if (platform === 'linux-cpu') {
   targetUri += CPU_LINUX;
 } else if (platform === 'linux-gpu') {
@@ -19,6 +25,16 @@ if (platform === 'linux-cpu') {
   throw new Error(`Unsupported platform: ${platform}`);
 }
 
-console.log(`* Target dir: ${targetDir}`);
+console.log(`* Target path: ${targetDir}`);
 console.log(`* Platform: ${platform}`);
 console.log(`* URI: ${targetUri}`);
+
+const test = fs.createWriteStream('test.download');
+const request = https.get(targetUri, (response) => {
+  response.pipe(test);
+  console.log(' --- piped');
+});
+request.on('close', () => {
+  console.log('.... Closed!');
+});
+request.end();
