238eb958bb16ebae145ae88ca5ec5d77037bcacd
Nick Kreeger <nick.kreeger@gmail.com>
2018-08-03 07:37:03 -0700

    Fix stack problem

--- binding/tfjs_binding.cc
+++ binding/tfjs_binding.cc
@@ -40,7 +40,7 @@ static napi_value CreateTensor(napi_env env, napi_callback_info info) {
   napi_status nstatus;
 
   // Create tensor takes 3 params: shape, dtype, typed-array:
-  size_t argc;
+  size_t argc = 3;
   napi_value args[3];
   napi_value js_this;
   nstatus = napi_get_cb_info(env, info, &argc, args, &js_this, nullptr);
@@ -62,7 +62,7 @@ static napi_value DeleteTensor(napi_env env, napi_callback_info info) {
   napi_status nstatus;
 
   // Delete tensor takes 1 param: tensor ID;
-  size_t argc;
+  size_t argc = 1;
   napi_value args[1];
   napi_value js_this;
   nstatus = napi_get_cb_info(env, info, &argc, args, &js_this, nullptr);
@@ -83,7 +83,7 @@ static napi_value TensorDataSync(napi_env env, napi_callback_info info) {
   napi_status nstatus;
 
   // Tensor data-sync takes 1 param: tensor ID;
-  size_t argc;
+  size_t argc = 1;
   napi_value args[1];
   napi_value js_this;
   nstatus = napi_get_cb_info(env, info, &argc, args, &js_this, nullptr);
@@ -104,7 +104,7 @@ static napi_value ExecuteOp(napi_env env, napi_callback_info info) {
 
   // Create tensor takes 3 params: op-name, op-attrs, input-tensor-ids,
   // num-outputs:
-  size_t argc;
+  size_t argc = 4;
   napi_value args[4];
   napi_value js_this;
   nstatus = napi_get_cb_info(env, info, &argc, args, &js_this, nullptr);
