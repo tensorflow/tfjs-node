a2c2c5c5b163f8032ff6d396ae66476ed31cbf41
Nick Kreeger <nick.kreeger@gmail.com>
2018-08-03 11:36:52 -0700

    save

--- scripts/get_libtensorflow.js
+++ scripts/get_libtensorflow.js
@@ -1,3 +1,19 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
@@ -17,7 +33,7 @@ const platform = process.argv[2];
 const targetDir = process.argv[3];
 
 // TODO(kreeger): Handle windows (dll) support:
-// https://github.com/tensorflow/tfjs/issues/548
+// https://github.com/tensorflow/tfjs/issues/549
 let targetUri = BASE_URI;
 let libName = 'libtensorflow';
 if (platform === 'linux-cpu') {
@@ -55,6 +71,27 @@ async function symlinkDepsLib() {
 }
 
 /**
+ * Downloads libtensorflow and optionally symlinks the library as needed.
+ */
+async function downloadLibtensorflow(shouldSymlink) {
+  // The deps folder and resources do not exist, download and symlink as
+  // needed:
+  console.error('  * Downloading libtensorflow');
+  const request = https.get(targetUri, response => {
+    response
+        .pipe(tar.x({
+          C: depsPath,
+        }))
+        .on('close', async () => {
+          if (shouldSymlink) {
+            await symlinkDepsLib();
+          }
+        });
+    request.end();
+  });
+}
+
+/**
  * Ensures libtensorflow requirements are met for building the binding.
  */
 async function run() {
@@ -62,29 +99,19 @@ async function run() {
   await ensureDir(depsPath);
 
   // This script can optionally only download and not symlink:
-  const shouldSymlink = destLibPath !== undefined;
-
-  if (!shouldSymlink || !await exists(destLibPath)) {
-    if (shouldSymlink && await exists(depsLibPath)) {
-      // Deps library exists, symlink to destination:
+  if (destLibPath !== undefined) {
+    if (await exists(depsLibPath)) {
+      // The libtensorflow package has already been downloaded, simply simlink
+      // to the destination path:
       await symlinkDepsLib();
     } else {
-      // Deps library does not exist, download resource package and symlink when
-      // unpacked.
-      console.error('  * Downloading libtensorflow');
-      const request = https.get(targetUri, response => {
-        response
-            .pipe(tar.x({
-              C: depsPath,
-            }))
-            .on('close', async () => {
-              if (shouldSymlink) {
-                await symlinkDepsLib();
-              }
-            });
-        request.end();
-      });
+      // The libtensorflow package does not exist, download and symlink to the
+      // destination path:
+      downloadLibtensorflow(true);
     }
+  } else {
+    // No symlink destination path supplied, simply download libtensorflow:
+    downloadLibtensorflow(false);
   }
 }
 
