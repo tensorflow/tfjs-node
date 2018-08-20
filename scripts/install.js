3b4412dae98276003077f7373f03964787abe97b
kangyizhang <kangyizhang@google.com>
2018-08-20 14:19:19 -0700

    save

--- scripts/install.js
+++ scripts/install.js
@@ -2,12 +2,11 @@ const cp = require('child_process');
 const os = require('os');
 
 // Skip the node-gyp rebuild step when building NPM tfjs-node-gpu package.
-if(!(process.argv[2] === 'compile-npm' && os.platform() !== 'linux')) {
+if(!(process.argv[2] === 'compile-npm' && os.platform() === 'linux')) {
   console.log('node-gyp rebuild');
   cp.exec('node-gyp rebuild', (err) => {
     if (err) {
-      // node couldn't execute the command
-      console.log(err);
+      console.log('node-gyp rebuild failed with: ' + err);
       return;
     }
   });
