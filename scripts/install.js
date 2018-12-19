d1e06f4ad4c60753c1b8164549c9490c448fe035
Nick Kreeger <nick.kreeger@gmail.com>
2018-12-19 10:42:43 -0800

    Upgrade mac/linux to TF 1.12

--- scripts/install.js
+++ scripts/install.js
@@ -35,9 +35,9 @@ const unlink = util.promisify(fs.unlink);
 const exec = util.promisify(cp.exec);
 
 const BASE_URI = 'https://storage.googleapis.com/tf-builds/';
-const CPU_DARWIN = 'libtensorflow_r1_11_darwin.tar.gz';
-const CPU_LINUX = 'libtensorflow_r1_11_linux_cpu.tar.gz';
-const GPU_LINUX = 'libtensorflow_r1_11_linux_gpu.tar.gz';
+const CPU_DARWIN = 'libtensorflow_r1_12_darwin.tar.gz';
+const CPU_LINUX = 'libtensorflow_r1_12_linux_cpu.tar.gz';
+const GPU_LINUX = 'libtensorflow_r1_12_linux_gpu.tar.gz';
 const CPU_WINDOWS = 'libtensorflow_r1_11_windows_cpu.zip';
 const GPU_WINDOWS = 'libtensorflow_r1_11_windows_gpu.zip';
 
