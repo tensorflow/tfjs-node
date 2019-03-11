cdfdab0ac386d6ecaed576a0f2f5d1224c433f7b
Shanqing Cai <cais@google.com>
2019-03-11 13:12:50 -0400

    Back out unwanted file

--- src/try_tensorboard.ts
+++
@@ -1,7 +0,0 @@
-import * as tf from './index';
-
-const summaryWriter = tf.node.summaryFileWriter('/tmp/tfjs_tb_logdir');
-for (let i = -1e3; i < 1e3; i += 10) {
-  summaryWriter.scalar('loss', i * i * i * i, i);
-  summaryWriter.scalar('acc', -i * i * i * i, i);
-}
