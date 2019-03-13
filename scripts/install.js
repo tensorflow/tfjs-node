f21faaca0c0502362d72f0a6357061d5e6dabfc3
Nick Kreeger <nick.kreeger@gmail.com>
2019-03-12 23:44:31 -0400

    1.13.1 instead of 1.13.0

--- scripts/install.js
+++ scripts/install.js
@@ -32,11 +32,11 @@ const unlink = util.promisify(fs.unlink);
 
 const BASE_URI =
     'https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-';
-const CPU_DARWIN = 'cpu-darwin-x86_64-1.13.0.tar.gz';
-const CPU_LINUX = 'cpu-linux-x86_64-1.13.0.tar.gz';
-const GPU_LINUX = 'gpu-linux-x86_64-1.13.0.tar.gz';
-const CPU_WINDOWS = 'cpu-windows-x86_64-1.13.0.zip';
-const GPU_WINDOWS = 'gpu-windows-x86_64-1.13.0.zip';
+const CPU_DARWIN = 'cpu-darwin-x86_64-1.13.1.tar.gz';
+const CPU_LINUX = 'cpu-linux-x86_64-1.13.1.tar.gz';
+const GPU_LINUX = 'gpu-linux-x86_64-1.13.1.tar.gz';
+const CPU_WINDOWS = 'cpu-windows-x86_64-1.13.1.zip';
+const GPU_WINDOWS = 'gpu-windows-x86_64-1.13.1.zip';
 
 // TODO(kreeger): Update to TensorFlow 1.13:
 // https://github.com/tensorflow/tfjs/issues/1369
