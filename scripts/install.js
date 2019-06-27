8cfa930029a01d449d52425acb498d744822028d
Shanqing Cai <cais@google.com>
2019-06-27 19:09:04 -0400

    Slightly improve install scripts by moving lib version to a const

--- scripts/install.js
+++ scripts/install.js
@@ -14,6 +14,9 @@
  * limitations under the License.
  * =============================================================================
  */
+
+import {getLibTensorFlowMajorDotMinorVersion, LIBTENSORFLOW_VERSION} from './deps-constants';
+
 const fs = require('fs');
 let path = require('path');
 const rimraf = require('rimraf');
@@ -31,16 +34,17 @@ const rimrafPromise = util.promisify(rimraf);
 
 const BASE_URI =
     'https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-';
-const CPU_DARWIN = 'cpu-darwin-x86_64-1.14.0.tar.gz';
-const CPU_LINUX = 'cpu-linux-x86_64-1.14.0.tar.gz';
-const GPU_LINUX = 'gpu-linux-x86_64-1.14.0.tar.gz';
-const CPU_WINDOWS = 'cpu-windows-x86_64-1.14.0.zip';
-const GPU_WINDOWS = 'gpu-windows-x86_64-1.14.0.zip';
+const CPU_DARWIN = `cpu-darwin-x86_64-${LIBTENSORFLOW_VERSION}.tar.gz`;
+const CPU_LINUX = `cpu-linux-x86_64-${LIBTENSORFLOW_VERSION}.tar.gz`;
+const GPU_LINUX = `gpu-linux-x86_64-${LIBTENSORFLOW_VERSION}.tar.gz`;
+const CPU_WINDOWS = `cpu-windows-x86_64-${LIBTENSORFLOW_VERSION}.zip`;
+const GPU_WINDOWS = `gpu-windows-x86_64-${LIBTENSORFLOW_VERSION}.zip`;
 
 // TODO(kreeger): Update to TensorFlow 1.13:
 // https://github.com/tensorflow/tfjs/issues/1369
 const TF_WIN_HEADERS_URI =
-    'https://storage.googleapis.com/tf-builds/tensorflow-headers-1.14.zip';
+    `https://storage.googleapis.com/tf-builds/tensorflow-headers-` +
+    `${getLibTensorFlowMajorDotMinorVersion()}.zip`;
 
 const platform = os.platform();
 let libType = process.argv[2] === undefined ? 'cpu' : process.argv[2];
