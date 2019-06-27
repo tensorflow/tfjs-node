9ed7b1f7a92d2227c072a3f65745c083623e9724
kangyizhang <kangyizhang@google.com>
2019-06-27 13:17:41 -0700

    save

---
+++ build-tmp-napi-v3/Release/.deps/Release/obj.target/tfjs_binding/binding/tfjs_backend.o.d
@@ -0,0 +1,23 @@
+cmd_Release/obj.target/tfjs_binding/binding/tfjs_backend.o := g++ '-DNODE_GYP_MODULE_NAME=tfjs_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/include/node -I/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/src -I/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/deps/openssl/config -I/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/deps/openssl/openssl/include -I/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/deps/uv/include -I/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/deps/zlib -I/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/deps/v8/include -I../.. -I/usr/local/google/home/kangyizhang/tensorflow/tfjs-node/deps/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/tfjs_binding/binding/tfjs_backend.o.d.raw   -c -o Release/obj.target/tfjs_binding/binding/tfjs_backend.o ../binding/tfjs_backend.cc
+Release/obj.target/tfjs_binding/binding/tfjs_backend.o: \
+ ../binding/tfjs_backend.cc ../binding/tfjs_backend.h \
+ /usr/local/google/home/kangyizhang/.node-gyp/10.15.3/include/node/node_api.h \
+ /usr/local/google/home/kangyizhang/.node-gyp/10.15.3/include/node/node_api_types.h \
+ /usr/local/google/home/kangyizhang/tensorflow/tfjs-node/deps/include/tensorflow/c/eager/c_api.h \
+ /usr/local/google/home/kangyizhang/tensorflow/tfjs-node/deps/include/tensorflow/c/c_api.h \
+ /usr/local/google/home/kangyizhang/tensorflow/tfjs-node/deps/include/tensorflow/c/tf_attrtype.h \
+ ../binding/napi_auto_ref.h ../binding/utils.h \
+ ../binding/tf_auto_status.h ../binding/tf_auto_tensor.h \
+ ../binding/tfe_auto_op.h
+../binding/tfjs_backend.cc:
+../binding/tfjs_backend.h:
+/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/include/node/node_api.h:
+/usr/local/google/home/kangyizhang/.node-gyp/10.15.3/include/node/node_api_types.h:
+/usr/local/google/home/kangyizhang/tensorflow/tfjs-node/deps/include/tensorflow/c/eager/c_api.h:
+/usr/local/google/home/kangyizhang/tensorflow/tfjs-node/deps/include/tensorflow/c/c_api.h:
+/usr/local/google/home/kangyizhang/tensorflow/tfjs-node/deps/include/tensorflow/c/tf_attrtype.h:
+../binding/napi_auto_ref.h:
+../binding/utils.h:
+../binding/tf_auto_status.h:
+../binding/tf_auto_tensor.h:
+../binding/tfe_auto_op.h:
---
+++ build-tmp-napi-v3/Release/.deps/usr/local/google/home/kangyizhang/tensorflow/tfjs-node/lib/napi-v3/tfjs_binding.node.d
@@ -0,0 +1,1 @@
+cmd_/usr/local/google/home/kangyizhang/tensorflow/tfjs-node/lib/napi-v3/tfjs_binding.node := rm -rf "/usr/local/google/home/kangyizhang/tensorflow/tfjs-node/lib/napi-v3/tfjs_binding.node" && cp -af "Release/tfjs_binding.node" "/usr/local/google/home/kangyizhang/tensorflow/tfjs-node/lib/napi-v3/tfjs_binding.node"
