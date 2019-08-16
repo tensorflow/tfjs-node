31391a407ceee7a69c7318a7785992b0e22fed52
Daniel Smilkov <dsmilkov@gmail.com>
2019-08-16 10:29:12 -0400

    save

--- scripts/test-travis.js
+++ scripts/test-travis.js
@@ -17,56 +17,71 @@
  */
 
 const fetch = require('node-fetch');
+const {exec} = require('./test-util');
+const shell = require('shelljs');
 
-const API_URL = 'https://api.travis-ci.org/repo/tensorflow%2Ftfjs-node';
-const SUBMIT_BUILD_URL = `${API_URL}/requests`;
-const GET_BUILD_INFO_URL = `${API_URL}/request`;
-const HEADERS = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'Travis-API-Version': '3',
-  'Authorization': 'token INKvoqirQIBD805HgU3Gew'
-};
+exec(
+    'git clone -depth=1 --single-branch ' +
+    'https://github.com/tensorflow/tfjs-node clone');
+const commitSha = process.env.COMMIT_SHA;
 
-async function submitBuild() {
-  const body = {
-    request: {
-      branch: process.env.BRANCH_NAME,
-    }
-  };
-  console.log('sending body', body);
-  const response = await fetch(SUBMIT_BUILD_URL, {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: HEADERS,
-  });
-  return response.json();
-}
+console.log('got commit', commitSha);
 
-async function getBuildInfo(requestId) {
-  console.log('Getting request info for', requestId);
-  const response = await fetch(`${GET_BUILD_INFO_URL}/${requestId}`, {
-    method: 'get',
-    headers: HEADERS,
-  });
-  return response.json();
-}
+shell.cd('clone');
+exec(`git fetch origin ${commitSha}`);
+exec(`git checkout -b ${commitSha} ${commitSha}`);
+exec(`git push --set-upstream origin ${commitSha}`);
 
-async function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-async function run() {
-  const requestInfo = await submitBuild();
-  console.log(requestInfo);
-  console.log('=======================================');
-  await sleep(30000);
-  const buildInfo = await getBuildInfo(requestInfo.request.id);
-  console.log(buildInfo);
-  const buildId = buildInfo.builds[0].id;
-  const slug = buildInfo.repository.slug;
-  const buildUrl = `https://travis-ci.org/${slug}/builds/${buildId}`;
-  console.log(`See build at: ${buildUrl}`);
-}
+// const API_URL = 'https://api.travis-ci.org/repo/tensorflow%2Ftfjs-node';
+// const SUBMIT_BUILD_URL = `${API_URL}/requests`;
+// const GET_BUILD_INFO_URL = `${API_URL}/request`;
+// const HEADERS = {
+//   'Content-Type': 'application/json',
+//   'Accept': 'application/json',
+//   'Travis-API-Version': '3',
+//   'Authorization': 'token INKvoqirQIBD805HgU3Gew'
+// };
 
-run();
+// async function submitBuild() {
+//   const body = {
+//     request: {
+//       branch: process.env.BRANCH_NAME,
+//     }
+//   };
+//   console.log('sending body', body);
+//   const response = await fetch(SUBMIT_BUILD_URL, {
+//     method: 'post',
+//     body: JSON.stringify(body),
+//     headers: HEADERS,
+//   });
+//   return response.json();
+// }
+
+// async function getBuildInfo(requestId) {
+//   console.log('Getting request info for', requestId);
+//   const response = await fetch(`${GET_BUILD_INFO_URL}/${requestId}`, {
+//     method: 'get',
+//     headers: HEADERS,
+//   });
+//   return response.json();
+// }
+
+// async function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// async function run() {
+//   const requestInfo = await submitBuild();
+//   console.log(requestInfo);
+//   console.log('=======================================');
+//   await sleep(30000);
+//   const buildInfo = await getBuildInfo(requestInfo.request.id);
+//   console.log(buildInfo);
+//   const buildId = buildInfo.builds[0].id;
+//   const slug = buildInfo.repository.slug;
+//   const buildUrl = `https://travis-ci.org/${slug}/builds/${buildId}`;
+//   console.log(`See build at: ${buildUrl}`);
+// }
+
+// run();
